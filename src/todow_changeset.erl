01debbb75ed8d2ae8f538d997b3dbbd8c5736d4a
William Fank Thome <willegp88@gmail.com>
2022-02-06 19:44:24 -0300

    feat: changeset record

--- src/todow_changeset.erl
+++ src/todow_changeset.erl
@@ -1,18 +1,85 @@
 -module(todow_changeset).
 
+-record(changeset, {
+  changes = maps:new() :: map(),
+  data = maps:new() :: map()
+}).
+-opaque changeset() :: #changeset{}.
+-export_type([ changeset/0 ]).
+
+-export([
+  new/2,
+  is_changeset/1,
+  get_changes/1, set_changes/2,
+  get_data/1, set_data/2
+]).
 -export([ cast/2, cast/3 ]).
+
+%%====================================================================
+%% Api
+%%====================================================================
+
+-spec new(Changes :: map(), Data :: map()) -> changeset().
+
+new(Changes, Data) -> #changeset{
+  changes = Changes,
+  data = Data
+}.
+
+-spec is_changeset(Unknown :: any()) -> boolean().
+
+is_changeset(Unknown) when is_record(Unknown, changeset) -> true;
+is_changeset(_Unknown) -> false.
+
+-spec get_changes(Changeset :: changeset()) -> map().
+
+get_changes(undefined) -> undefined;
+get_changes(#changeset{changes = Changes}) -> Changes.
+
+-spec set_changes(Changeset :: changeset(), Changes :: map()) ->  changeset().
+
+set_changes(Changeset, Changes) ->
+  Changeset#changeset{changes = Changes}.
+
+-spec get_data(Changeset :: changeset()) -> map().
+
+get_data(undefined) -> undefined;
+get_data(#changeset{data = Data}) -> Data.
+
+-spec set_data(Changeset :: changeset(), Data :: map()) ->  changeset().
+
+set_data(Changeset, Data) ->
+  Changeset#changeset{data = Data}.
+
+-spec cast(Data :: map(), Params :: map()) -> changeset().
 
 cast(Data, Params) ->
   cast(Data, Params, []).
 
+-spec cast(Data :: map(), Params :: map(), Permitted :: [atom()]) -> changeset().
+
 cast(Data, Params, Permitted) ->
   maps:fold(
-    fun(Key, Value, Acc) ->
+    fun(Key, Value, Changeset) ->
       case Permitted =:= [] orelse lists:member(Key, Permitted) of
-        true -> maps:put(Key, Value, Acc);
-        false -> Acc
+        true ->
+          NewData = maps:put(Key, Value, Changeset#changeset.data),
+          NewChanges = maps:put(Key, Value, Changeset#changeset.changes),
+          Changeset#changeset{
+            data = NewData,
+            changes = NewChanges
+          };
+        false -> Changeset
       end
     end,
-    Data,
+    #changeset{
+      data = Data
+    },
     Params
   ).
+
+%%====================================================================
+%% Internal functions
+%%====================================================================
+
+%% nothing here
--- test/todow_changeset_tests.erl
+++ test/todow_changeset_tests.erl
@@ -1,14 +1,40 @@
 -module(todow_changeset_tests).
 
 -import(todow_changeset, [
+  new/2,
   cast/2, cast/3
 ]).
 
 -include_lib("eunit/include/eunit.hrl").
 
 cast_test() ->
-  Data = #{},
-  Params = #{foo => bar, bar => baz},
-  Permitted = [foo],
-  ?assertEqual(#{foo => bar, bar => baz}, cast(Data, Params)),
-  ?assertEqual(#{foo => bar}, cast(Data, Params, Permitted)).
+  Changeset0 = cast(
+    #{},
+    #{foo => bar, bar => baz}
+  ),
+  ExpectedChangeset0 = new(
+    #{foo => bar, bar => baz},
+    #{foo => bar, bar => baz}
+  ),
+  ?assertEqual(ExpectedChangeset0, Changeset0),
+
+  Changeset1 = cast(
+    #{},
+    #{foo => bar, bar => baz},
+    [foo]
+  ),
+  ExpectedChangeset1 = new(
+    #{foo => bar},
+    #{foo => bar}
+  ),
+  ?assertEqual(ExpectedChangeset1, Changeset1),
+
+  Changeset2 = cast(
+    #{foo => bar},
+    #{bar => baz}
+  ),
+  ExpectedChangeset2 = new(
+    #{bar => baz},
+    #{foo => bar, bar => baz}
+  ),
+  ?assertEqual(ExpectedChangeset2, Changeset2).
