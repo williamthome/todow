4efaba2dfcd542bc3249ec383ae9eeb2a16b2a69
William Fank Thome <willegp88@gmail.com>
2022-02-20 11:02:07 -0300

    refactor: new changeset strategy

--- src/todow_changeset.erl
+++ src/todow_changeset.erl
@@ -1,30 +1,45 @@
 -module(todow_changeset).
 
+-type action() :: new | update | undefined.
+
 -record(changeset, {
+  data = maps:new() :: map(),
   changes = maps:new() :: map(),
-  data = maps:new() :: map()
+  action :: action()
 }).
 -opaque changeset() :: #changeset{}.
--export_type([ changeset/0 ]).
+
+-export_type([ changeset/0, action/0 ]).
 
 -export([
-  new/2,
+  new/2, new/3,
   is_changeset/1,
   get_changes/1, set_changes/2,
-  get_data/1, set_data/2
+  get_data/1, set_data/2,
+  get_action/1, set_action/2
 ]).
--export([ cast/2, cast/3 ]).
+-export([ cast/3, cast/4 ]).
+
+-define(is_cast_action(Action), Action =:= new orelse Action =:= update).
 
 %%====================================================================
 %% Api
 %%====================================================================
 
--spec new(Changes :: map(), Data :: map()) -> changeset().
+-spec new(Data :: map(), Changes :: map()) -> {ok, changeset()}.
 
-new(Changes, Data) -> #changeset{
-  changes = Changes,
-  data = Data
-}.
+new(Data, Changes) ->
+  new(Data, Changes, guess_action(Data)).
+
+-spec new(Data :: map(), Changes :: map(), Action :: action()) -> {ok, changeset()}.
+
+new(Data, Changes, Action) ->
+  Changeset = #changeset{
+    data = Data,
+    changes = Changes,
+    action = Action
+  },
+  {ok, Changeset}.
 
 -spec is_changeset(Unknown :: any()) -> boolean().
 
@@ -38,8 +53,7 @@ get_changes(#changeset{changes = Changes}) -> Changes.
 
 -spec set_changes(Changeset :: changeset(), Changes :: map()) ->  changeset().
 
-set_changes(Changeset, Changes) ->
-  Changeset#changeset{changes = Changes}.
+set_changes(Changeset, Changes) -> Changeset#changeset{changes = Changes}.
 
 -spec get_data(Changeset :: changeset()) -> map().
 
@@ -48,38 +62,70 @@ get_data(#changeset{data = Data}) -> Data.
 
 -spec set_data(Changeset :: changeset(), Data :: map()) ->  changeset().
 
-set_data(Changeset, Data) ->
-  Changeset#changeset{data = Data}.
+set_data(Changeset, Data) -> Changeset#changeset{data = Data}.
 
--spec cast(Data :: map(), Params :: map()) -> changeset().
+-spec get_action(Changeset :: changeset()) -> action().
 
-cast(Data, Params) ->
-  cast(Data, Params, []).
+get_action(undefined) -> undefined;
+get_action(#changeset{action = Action}) -> Action.
 
--spec cast(Data :: map(), Params :: map(), Permitted :: [atom()]) -> changeset().
+-spec set_action(Changeset :: changeset(), Action :: action()) ->  changeset().
 
-cast(Data, Params, Permitted) ->
-  maps:fold(
-    fun(Key, Value, Changeset) ->
-      case Permitted =:= [] orelse lists:member(Key, Permitted) of
-        true ->
-          NewData = maps:put(Key, Value, Changeset#changeset.data),
-          NewChanges = maps:put(Key, Value, Changeset#changeset.changes),
-          Changeset#changeset{
-            data = NewData,
-            changes = NewChanges
-          };
-        false -> Changeset
-      end
-    end,
-    #changeset{
-      data = Data
-    },
-    Params
-  ).
+set_action(Changeset, Action) -> Changeset#changeset{action = Action}.
+
+-spec cast(Data :: map(), Changes :: map(), ValidKeys :: list()) -> changeset().
+
+cast(Data, Changes, ValidKeys) -> cast(Data, Changes, ValidKeys, #{}).
+
+-spec cast(
+  Data :: map(), Changes :: map(), ValidKeys :: list(), Options :: map()
+) -> changeset().
+
+cast(Data, Changes, ValidKeys, Options)
+  when is_map(Changes)
+  andalso is_list(ValidKeys)
+  andalso is_map(Options) ->
+    Action = guess_action(Data),
+    Defaults = maps:get(defaults, Options, maps:new()),
+    Changeset = maps:fold(
+      fun(Key, Value, ChangesetAcc) ->
+        case lists:member(Key, ValidKeys) of
+          true ->
+            OldValue = maps:get(Key, Data, undefined),
+            NewValue = maybe_default(Key, Value, Defaults),
+            maybe_set_change(ChangesetAcc, Key, OldValue, NewValue);
+          false -> ChangesetAcc
+        end
+      end,
+      #changeset{
+        action = Action,
+        data = Data
+      },
+      maybe_merge_defaults(Action, Changes, Defaults)
+    ),
+    {ok, Changeset}.
 
 %%====================================================================
 %% Internal functions
 %%====================================================================
 
-%% nothing here
+guess_action(Data) when map_size(Data) == 0 -> new;
+guess_action(_Data) -> update.
+
+maybe_merge_defaults(new, Changes, Defaults) -> maps:merge(Defaults, Changes);
+maybe_merge_defaults(update, Changes, _Defaults) -> Changes.
+
+maybe_default(Key, undefined, Defaults) -> maps:get(Key, Defaults, undefined);
+maybe_default(_Key, Value, _Defaults) -> Value.
+
+set_change(Changeset, Key, Value) ->
+  Changes = maps:put(Key, Value, get_changes(Changeset)),
+  Data = maps:put(Key, Value, get_data(Changeset)),
+  Changeset#changeset{
+    changes = Changes,
+    data = Data
+  }.
+
+maybe_set_change(Changeset, _Key, NewValue, NewValue) -> Changeset;
+maybe_set_change(Changeset, Key, _OldValue, NewValue) ->
+  set_change(Changeset, Key, NewValue).
--- test/todow_changeset_tests.erl
+++ test/todow_changeset_tests.erl
@@ -1,8 +1,8 @@
 -module(todow_changeset_tests).
 
 -import(todow_changeset, [
-  new/2,
-  cast/2, cast/3
+  new/3,
+  cast/3, cast/4
 ]).
 
 -include_lib("eunit/include/eunit.hrl").
@@ -10,11 +10,13 @@
 cast_test() ->
   Changeset0 = cast(
     #{},
-    #{foo => bar, bar => baz}
+    #{foo => bar, bar => baz},
+    [foo, bar]
   ),
   ExpectedChangeset0 = new(
     #{foo => bar, bar => baz},
-    #{foo => bar, bar => baz}
+    #{foo => bar, bar => baz},
+    new
   ),
   ?assertEqual(ExpectedChangeset0, Changeset0),
 
@@ -25,16 +27,19 @@ cast_test() ->
   ),
   ExpectedChangeset1 = new(
     #{foo => bar},
-    #{foo => bar}
+    #{foo => bar},
+    new
   ),
   ?assertEqual(ExpectedChangeset1, Changeset1),
 
   Changeset2 = cast(
     #{foo => bar},
-    #{bar => baz}
+    #{bar => baz},
+    [foo, bar]
   ),
   ExpectedChangeset2 = new(
+    #{foo => bar, bar => baz},
     #{bar => baz},
-    #{foo => bar, bar => baz}
+    update
   ),
   ?assertEqual(ExpectedChangeset2, Changeset2).
