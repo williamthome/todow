d0d6f707d9cf5ee270d7b6bea8e6706b7accfbd2
William Fank Thome <willegp88@gmail.com>
2022-03-06 10:36:56 -0300

    refactor: do not inspect if production env

--- src/include/todow.hrl
+++ src/include/todow.hrl
@@ -1,1 +1,10 @@
--define(INSPECT(What), begin io:format("===\n[~p:~p] ~p\n===\n", [?MODULE, ?LINE, What]), What end).
+-ifdef(PROD).
+-define(INSPECT(What), What).
+-else.
+-define(INSPECT(What),
+  begin
+    io:format("===\n[~p:~p] ~p\n===\n", [?MODULE, ?LINE, What]),
+    What
+  end
+).
+-endif.
