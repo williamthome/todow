3441918d83648458fb29d9fa7730d15f274d28af
William Fank Thome <willegp88@gmail.com>
2022-03-17 21:17:37 -0300

    feat: schema constructor

--- src/todow_schema.erl
+++ src/todow_schema.erl
@@ -1,9 +1,50 @@
+%%%-----------------------------------------------------------------------------
+%%% @copyright (C) 2022, williamthome
+%%% @doc Schema for db module.
+%%%
+%%% @author William Fank Thomé
+%%% @end
+%%%-----------------------------------------------------------------------------
 -module(todow_schema).
 
+-ifdef(TEST).
+-include_lib("eunit/include/eunit.hrl").
+-endif.
+
+-type name() :: atom().
+-type fields() :: nonempty_list(todow_field:t()).
+
 -record(schema, {
-    name :: atom(),
-    fields :: nonempty_list(todow_field:t())
+    name :: name(),
+    fields :: fields()
 }).
 -opaque t() :: #schema{}.
 
 -export_type([t/0]).
+-export([
+    new/2
+]).
+
+%%------------------------------------------------------------------------------
+%% @doc Schema constructor.
+%% @end
+%%------------------------------------------------------------------------------
+
+-spec new(Name :: name(), Fields :: fields()) -> t().
+
+new(Name, Fields) ->
+    #schema{
+        name = Name,
+        fields = Fields
+    }.
+
+%%%=============================================================================
+%%% Tests
+%%%=============================================================================
+
+-ifdef(TEST).
+
+new_test() ->
+    ?assertEqual(new(foo, []), #schema{name = foo, fields = []}).
+
+-endif.
