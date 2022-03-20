6d2908150776ab2af43f7dce3a39b0f72ea2b464
William Fank Thome <willegp88@gmail.com>
2022-03-20 14:28:02 -0300

    chore: create todow script

---
+++ todow.sh
@@ -0,0 +1,68 @@
+#!/bin/sh
+
+set -e
+
+case $1 in
+  ""|shell)
+    rebar3 shell
+  ;;
+  compile)
+    rebar3 compile
+  ;;
+  clean)
+    rebar3 clean
+  ;;
+  format)
+    rebar3 fmt
+  ;;
+  test)
+    case $2 in
+      ""|unit)
+        rebar3 eunit
+      ;;
+      integration)
+        rebar3 ct
+      ;;
+      all)
+        $0 test unit && $0 test integration
+      ;;
+      *)
+      echo "
+--------------------------------------------------------------------------------
+Error:
+    $2 is invalid.
+
+Valid options:
+    unit          Run unit tests.
+    integration   Run integration tests.
+    all           Run unit an integration tests.
+
+    If none is passed it runs unit tests.
+--------------------------------------------------------------------------------
+"
+      exit 1
+    ;;
+    esac
+  ;;
+  *)
+    echo "
+--------------------------------------------------------------------------------
+Error:
+    $1 is invalid.
+
+Valid options:
+    compile       Compile the needed dependencies and the project's
+                  apps .app.src and .erl files.
+    clean         Removes compiled beam files from apps.
+    shell         Runs a shell with project apps and deps in path.
+    format        Format all erlang files.
+    test          Pass 'unit', 'integration' or 'all' flag to test.
+
+    If none is passed it runs shell.
+--------------------------------------------------------------------------------
+"
+    exit 1
+  ;;
+esac
+
+exit 0
