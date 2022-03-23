49cb0b927eb960c3240906170751669e001fa200
William Fank Thome <willegp88@gmail.com>
2022-03-22 22:43:20 -0300

    refactor: apply identation

--- src/include/todow.hrl
+++ src/include/todow.hrl
@@ -7,17 +7,17 @@
 -else.
 -ifdef(TEST).
 -define(INSPECT(What),
-  begin
-    ?debugFmt("\n===\n[~p:~p] ~p\n===\n", [?MODULE, ?LINE, What]),
-    What
-  end
+    begin
+        ?debugFmt("\n===\n[~p:~p] ~p\n===\n", [?MODULE, ?LINE, What]),
+        What
+    end
 ).
 -else.
 -define(INSPECT(What),
-  begin
-    io:format("\n===\n[~p:~p] ~p\n===\n", [?MODULE, ?LINE, What]),
-    What
-  end
+    begin
+        io:format("\n===\n[~p:~p] ~p\n===\n", [?MODULE, ?LINE, What]),
+        What
+    end
 ).
 -endif.
 -endif.
