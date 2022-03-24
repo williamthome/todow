6a17c5548e93c272f7159d2c8812ebb8e4afc398
William Fank Thome <willegp88@gmail.com>
2022-03-23 23:02:38 -0300

    feat: add more types validation

--- src/todow_validation.erl
+++ src/todow_validation.erl
@@ -14,6 +14,35 @@
     is_number(Value) andalso Value >= Min andalso Value =< Max
 ).
 
+-define(is_year(Y), is_integer(Y)).
+-define(is_month(M), is_integer(M) andalso M >= 1 andalso M =< 12).
+-define(is_day(D), is_integer(D) andalso D >= 1 andalso D =< 31).
+-define(is_date(Date),
+    is_tuple(Date) andalso
+    tuple_size(Date) =:= 3 andalso
+    ?is_year(element(1, Date)) andalso
+    ?is_month(element(2, Date)) andalso
+    ?is_day(element(3, Date))
+).
+
+-define(is_hour(H), is_integer(H) andalso H >= 0 andalso H < 24).
+-define(is_minute(M), is_integer(M) andalso M >= 0 andalso M < 60).
+-define(is_second(S), is_integer(S) andalso S >= 0 andalso S < 60).
+-define(is_time(Time),
+    is_tuple(Time) andalso
+    tuple_size(Time) =:= 3 andalso
+    ?is_hour(element(1, Time)) andalso
+    ?is_minute(element(2, Time)) andalso
+    ?is_second(element(3, Time))
+).
+
+-define(is_datetime(DateTime),
+    is_tuple(DateTime) andalso
+    tuple_size(DateTime) =:= 2 andalso
+    ?is_date(element(1, DateTime)) andalso
+    ?is_time(element(2, DateTime))
+).
+
 -type validates_ok() :: {ok, any()}.
 -type validates_error() :: {error, {atom(), any(), string()}}.
 -type validates_result() :: validates_ok() | validates_error().
@@ -37,12 +66,24 @@
     validates_required/1,
     validates_is_integer/1,
     validates_is_binary/1,
+    validates_is_boolean/1,
+    validates_is_number/1,
+    validates_is_float/1,
+    validates_is_date/1,
+    validates_is_time/1,
+    validates_is_datetime/1,
     validates_range/3
 ]).
 -export([
     required_validation/0,
     is_integer_validation/0,
     is_binary_validation/0,
+    is_boolean_validation/0,
+    is_number_validation/0,
+    is_float_validation/0,
+    is_date_validation/0,
+    is_time_validation/0,
+    is_datetime_validation/0,
     range_validation/2
 ]).
 -export([validate/2]).
@@ -139,6 +180,138 @@ validates_is_binary(Value) ->
 -spec is_binary_validation() -> validates().
 
 is_binary_validation() -> fun validates_is_binary/1.
+
+%%------------------------------------------------------------------------------
+%% @doc Validates if the value is of boolean type.
+%% @end
+%%------------------------------------------------------------------------------
+
+-spec validates_is_boolean(Value :: any()) -> validates_result().
+
+validates_is_boolean(Value) when is_boolean(Value) ->
+    ?OK(Value);
+validates_is_boolean(Value) ->
+    Msg = "Value must be a boolean.",
+    ?ERROR({is_boolean, Value, Msg}).
+
+%%------------------------------------------------------------------------------
+%% @doc Validates is boolean constructor.
+%% @end
+%%------------------------------------------------------------------------------
+
+-spec is_boolean_validation() -> validates().
+
+is_boolean_validation() -> fun validates_is_boolean/1.
+
+%%------------------------------------------------------------------------------
+%% @doc Validates if the value is of number type.
+%% @end
+%%------------------------------------------------------------------------------
+
+-spec validates_is_number(Value :: any()) -> validates_result().
+
+validates_is_number(Value) when is_number(Value) ->
+    ?OK(Value);
+validates_is_number(Value) ->
+    Msg = "Value must be a number.",
+    ?ERROR({is_number, Value, Msg}).
+
+%%------------------------------------------------------------------------------
+%% @doc Validates is number constructor.
+%% @end
+%%------------------------------------------------------------------------------
+
+-spec is_number_validation() -> validates().
+
+is_number_validation() -> fun validates_is_number/1.
+
+%%------------------------------------------------------------------------------
+%% @doc Validates if the value is of float type.
+%% @end
+%%------------------------------------------------------------------------------
+
+-spec validates_is_float(Value :: any()) -> validates_result().
+
+validates_is_float(Value) when is_float(Value) ->
+    ?OK(Value);
+validates_is_float(Value) ->
+    Msg = "Value must be a float.",
+    ?ERROR({is_float, Value, Msg}).
+
+%%------------------------------------------------------------------------------
+%% @doc Validates is float constructor.
+%% @end
+%%------------------------------------------------------------------------------
+
+-spec is_float_validation() -> validates().
+
+is_float_validation() -> fun validates_is_float/1.
+
+%%------------------------------------------------------------------------------
+%% @doc Validates if the value is of date type.
+%% @end
+%%------------------------------------------------------------------------------
+
+-spec validates_is_date(Value :: any()) -> validates_result().
+
+validates_is_date(Value) when ?is_date(Value) ->
+    ?OK(Value);
+validates_is_date(Value) ->
+    Msg = "Value must be a date.",
+    ?ERROR({is_date, Value, Msg}).
+
+%%------------------------------------------------------------------------------
+%% @doc Validates is date constructor.
+%% @end
+%%------------------------------------------------------------------------------
+
+-spec is_date_validation() -> validates().
+
+is_date_validation() -> fun validates_is_date/1.
+
+%%------------------------------------------------------------------------------
+%% @doc Validates if the value is of time type.
+%% @end
+%%------------------------------------------------------------------------------
+
+-spec validates_is_time(Value :: any()) -> validates_result().
+
+validates_is_time(Value) when ?is_time(Value) ->
+    ?OK(Value);
+validates_is_time(Value) ->
+    Msg = "Value must be a time.",
+    ?ERROR({is_time, Value, Msg}).
+
+%%------------------------------------------------------------------------------
+%% @doc Validates is time constructor.
+%% @end
+%%------------------------------------------------------------------------------
+
+-spec is_time_validation() -> validates().
+
+is_time_validation() -> fun validates_is_time/1.
+
+%%------------------------------------------------------------------------------
+%% @doc Validates if the value is of datetime type.
+%% @end
+%%------------------------------------------------------------------------------
+
+-spec validates_is_datetime(Value :: any()) -> validates_result().
+
+validates_is_datetime(Value) when ?is_datetime(Value) ->
+    ?OK(Value);
+validates_is_datetime(Value) ->
+    Msg = "Value must be a datetime.",
+    ?ERROR({is_datetime, Value, Msg}).
+
+%%------------------------------------------------------------------------------
+%% @doc Validates is datetime constructor.
+%% @end
+%%------------------------------------------------------------------------------
+
+-spec is_datetime_validation() -> validates().
+
+is_datetime_validation() -> fun validates_is_datetime/1.
 
 %%------------------------------------------------------------------------------
 %% @doc Validates number range.
