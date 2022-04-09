22f47bd5a31092df985acfed657383b2af158a18
William Fank Thome <willegp88@gmail.com>
2022-04-09 03:55:33 -0300

    feat: todow_param

---
+++ src/todow_param.erl
@@ -0,0 +1,164 @@
+-module(todow_param).
+
+-include("./include/todow.hrl").
+
+-define(PARAM_DEFAULTS, #{
+    default => undefined,
+    validators => []
+}).
+
+-define(is_param(X), is_map(X)).
+
+-type name() :: atom().
+% TODO: More types
+-type type() ::
+    atom
+    | integer
+    | float
+    | number
+    | binary
+    | boolean
+    | date
+    | time
+    | datetime
+    | currency.
+% TODO: More error codes
+-type validation_error_code() ::
+    bad_arg
+    | unauthorized
+    | forbidden
+    | internal_server_error.
+-type validation_error_reason() :: atom().
+-type validation_error() :: {
+    Code :: validation_error_code(),
+    Reason :: validation_error_reason(),
+    Value :: any()
+}.
+-type validates() :: fun((1) -> ok | {error, validation_error()}).
+-type validators() :: validates() | list(validates()).
+-type default() :: fun(() -> any()) | any().
+
+% TODO: Cast?
+-type t() :: #{
+    name => name(),
+    type => type(),
+    default => default(),
+    validators => validators()
+}.
+
+-export_type([
+    t/0
+]).
+
+-export([
+    new/1,
+    merge/2
+]).
+
+-spec new(Args :: map()) -> t().
+
+% TODO: Add validators by type
+new(Args = #{name := _Name, type := Type}) ->
+    Defaults = ?PARAM_DEFAULTS#{type => Type},
+    todow_utils:factory(Args, Defaults, fun merge/2).
+
+-spec merge(A :: map(), B :: map()) -> t().
+
+merge(A = #{type := Type}, B = #{type := Type}) ->
+    A#{
+        default => merge_default(A, B),
+        validators => merge_validators(A, B)
+    }.
+
+%%%=============================================================================
+%%% Internal functions
+%%%=============================================================================
+
+-spec unwrap_default(Def :: any()) -> any().
+
+unwrap_default(#{default := Def}) -> unwrap_default(Def);
+unwrap_default(#{}) -> undefined;
+unwrap_default(Def) when is_function(Def, 0) -> Def();
+unwrap_default(Def) -> Def.
+
+-spec merge_default(DefA :: map(), DefB :: map()) -> any().
+
+merge_default(#{default := undefined}, #{default := DefB}) -> DefB;
+merge_default(#{default := DefA}, _DefB) when DefA =/= undefined -> DefA;
+merge_default(#{}, #{default := DefB}) -> DefB;
+merge_default(A, B) ->
+    fun() ->
+        case unwrap_default(A) of
+            undefined -> unwrap_default(B);
+            Def -> Def
+        end
+    end.
+
+-spec merge_validators(A :: t(), B :: t()) -> validators().
+
+merge_validators(A, B) ->
+    GetValidators = todow_utils:wrap_get(validators, []),
+    todow_utils:merge_to_list(GetValidators(B), GetValidators(A)).
+
+%%%=============================================================================
+%%% Tests
+%%%=============================================================================
+
+-ifdef(TEST).
+
+unwrap_default_test() ->
+    ?assertEqual(foo, unwrap_default(#{default => foo})),
+    ?assertEqual(foo, unwrap_default(#{default => fun() -> foo end})).
+
+merge_default_test() ->
+    Def1 = merge_default(#{default => foo}, #{default => fun() -> bar end}),
+    Def2 = merge_default(#{default => undefined}, #{default => fun() -> bar end}),
+    ?assertEqual(foo, unwrap_default(Def1)),
+    ?assertEqual(bar, unwrap_default(Def2)).
+
+merge_validators_test() ->
+    V1 = fun(_) -> ok end,
+    V2 = fun(X) -> {error, {unknown, v2_error, X}} end,
+    V3 = fun(X) -> {error, {unknown, v3_error, X}} end,
+    A = #{validators => V1},
+    B = #{validators => [V2, V3]},
+    ?assertEqual([V2, V3, V1], merge_validators(A, B)).
+
+merge_test() ->
+    V1 = fun(_) -> ok end,
+    V2 = fun(X) -> {error, {unknown, v2_error, X}} end,
+    V3 = fun(X) -> {error, {unknown, v3_error, X}} end,
+    A = #{
+        name => foo,
+        type => any_type,
+        default => undefined,
+        validators => V1
+    },
+    B = #{
+        name => bar,
+        type => any_type,
+        default => baz,
+        validators => [V2, V3]
+    },
+    Expected = #{
+        name => foo,
+        type => any_type,
+        default => baz,
+        validators => [V2, V3, V1]
+    },
+    ?assertEqual(Expected, merge(A, B)).
+
+new_test() ->
+    Args = #{
+        name => foo,
+        type => any_type
+    },
+    Expected = #{
+        name => foo,
+        type => any_type,
+        default => undefined,
+        validators => []
+    },
+    ?assertEqual(Expected, new(Args)).
+
+-endif.
--- src/utils/todow_utils.erl
+++ src/utils/todow_utils.erl
@@ -2,8 +2,19 @@
 
 -include("../include/todow.hrl").
 
--export([ maybe_default/2, maybe_default/3, maybe_default/4 ]).
+-type prop() :: map() | proplists:proplist().
 
+-export([
+  maybe_default/2, maybe_default/3, maybe_default/4,
+  get/2, get/3,
+  get_else/2, get_else/3,
+  wrap_get/1, wrap_get/2,
+  remove/2,
+  merge_to_list/2,
+  factory/3, factory/4
+]).
+
+% TODO: Rename maybe_default to case_else, if_else or something like that
 -spec maybe_default(Value :: any(), Default :: any()) -> any().
 
 maybe_default(undefined, Default) -> Default;
@@ -25,6 +36,91 @@ maybe_default_is_function(Default, Args) when is_function(Default, 1) ->
   Default(Args);
 maybe_default_is_function(Default, _Args) -> Default.
 
+-spec get(Key :: any(), From :: prop()) -> any().
+
+get(Key, From) -> get(Key, From, undefined).
+
+-spec get(
+    Key :: any(), From :: prop(), Default :: any()
+) -> any().
+
+get(Key, Map, Default) when is_map(Map) ->
+    maybe_default(maps:get(Key, Map, Default), Default);
+get(Key, Proplist, Default) when is_list(Proplist) ->
+    maybe_default(proplists:get_value(Key, Proplist, Default), Default).
+
+-spec get_else(
+    Key :: any(), A :: prop(), B :: any()
+) -> any().
+
+get_else(Key, A, B) -> get_else(Key, [A, B]).
+
+-spec get_else(Key :: any(), From :: any()) -> any().
+
+get_else(_Key, []) ->
+    undefined;
+get_else(Key, [undefined | B]) ->
+    get_else(Key, B);
+get_else(Key, [A | B]) when is_map(A) orelse is_list(A) ->
+    case get(Key, A) of
+        undefined -> get_else(Key, B);
+        Found -> Found
+    end;
+get_else(_Key, Else) ->
+    Else.
+
+-spec wrap_get(Key :: any()) -> fun((Map :: map()) -> any()).
+
+wrap_get(Key) -> wrap_get(Key, undefined).
+
+-spec wrap_get(Key :: any(), Default :: any()) -> fun((Map :: map()) -> any()).
+
+wrap_get(Key, Default) -> fun(From) -> get(Key, From, Default) end.
+
+-spec remove(Key :: any(), Prop :: prop()) -> prop().
+
+remove(Key, Map) when is_map(Map) ->
+    maps:remove(Key, Map);
+remove(Key, Proplist) when is_list(Proplist) ->
+    proplists:delete(Key, Proplist).
+
+-spec merge_to_list(A :: any, B :: any) -> list().
+
+merge_to_list(A, B) when is_list(A) andalso is_list(B) ->
+    A ++ B;
+merge_to_list(A, B) when is_list(B) ->
+    [A] ++ B;
+merge_to_list(A, B) when is_list(A) ->
+    A ++ [B];
+merge_to_list(A, B) ->
+    [B, A].
+
+-spec factory(
+    Args :: prop(),
+    Defaults :: prop(),
+    MergeFun :: fun((A :: prop(), B :: prop()) -> prop())
+) -> prop().
+
+factory(Args, Defaults, MergeFun) ->
+    factory(Args, Defaults, merge, MergeFun).
+
+-spec factory(
+    Args :: prop(),
+    Defaults :: prop(),
+    MergeKey :: any(),
+    MergeFun :: fun((A :: prop(), B :: prop()) -> prop())
+) -> prop().
+
+factory(Args, Defaults, MergeKey, MergeFun) ->
+    ToMerge =
+        case todow_utils:get(MergeKey, Args, []) of
+            X when is_list(X) -> X;
+            X -> [X]
+        end,
+    ToMerge1 = lists:foldl(MergeFun, Defaults, ToMerge),
+    Args1 = remove(MergeKey, Args),
+    MergeFun(Args1, ToMerge1).
+
 %%%=============================================================================
 %%% Tests
 %%%=============================================================================
@@ -44,5 +140,44 @@ maybe_default_test() ->
   % maybe_default/4
   ?assertEqual(bar, maybe_default(foo, undefined, #{foo => fun(Args) -> Args end}, bar)),
   ?assertEqual(bar, maybe_default(foo, bar, #{foo => fun(Args) -> Args end}, foo)).
+
+get_test() ->
+    ?assertEqual(bar, get(foo, #{foo => bar})),
+    ?assertEqual(undefined, get(foo, #{bar => baz})),
+    ?assertEqual(bar, get(foo, #{foo => undefined}, bar)).
+
+get_else_test() ->
+    ?assertEqual(bar, get_else(foo, #{foo => bar}, #{foo => baz})),
+    ?assertEqual(baz, get_else(foo, #{foo => undefined}, #{foo => baz})),
+    ?assertEqual(undefined, get_else(bar, #{foo => any}, #{foo => any})).
+
+wrap_get_test() ->
+    Get = wrap_get(foo),
+    GetDef = wrap_get(foo, bar),
+    ?assertEqual(bar, Get(#{foo => bar})),
+    ?assertEqual(undefined, Get(#{bar => any})),
+    ?assertEqual(bar, GetDef(#{foo => undefined})).
+
+remove_test() ->
+    ?assertEqual(#{}, remove(foo, #{foo => bar})),
+    ?assertEqual(#{}, remove(foo, #{})),
+    ?assertEqual([], remove(foo, [{foo, bar}])),
+    ?assertEqual([], remove(foo, [])).
+
+merge_to_list_test() ->
+    ?assertEqual([], merge_to_list([], [])),
+    ?assertEqual([foo], merge_to_list([foo], [])),
+    ?assertEqual([foo], merge_to_list([], [foo])),
+    ?assertEqual([foo, bar], merge_to_list([foo], [bar])),
+    ?assertEqual([foo, bar], merge_to_list(foo, [bar])),
+    ?assertEqual([foo, bar], merge_to_list([foo], bar)).
+
+factory_test() ->
+    Expected = #{foo => foo, bar => bar, baz => baz},
+    Args = #{bar => bar, merge => #{baz => baz}},
+    Defaults = #{foo => foo},
+    MergeKey = merge,
+    MergeFun = fun maps:merge/2,
+    ?assertEqual(Expected, factory(Args, Defaults, MergeKey, MergeFun)).
 
 -endif.
